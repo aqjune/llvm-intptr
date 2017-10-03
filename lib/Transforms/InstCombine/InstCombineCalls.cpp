0c792909fd28a2a3e52ef1d2e24c0165e6590c3f
aqjune <aqjune@gmail.com>
2017-10-04 01:21:18 +0900

    Add psub optimization

--- lib/Transforms/InstCombine/InstCombineCalls.cpp
+++ lib/Transforms/InstCombine/InstCombineCalls.cpp
@@ -3715,6 +3715,15 @@ Instruction *InstCombiner::visitCallInst(CallInst &CI) {
     }
     break;
   }
+
+  case Intrinsic::psub: {
+    Value *Op1 = II->getArgOperand(0);
+    Value *Op2 = II->getArgOperand(1);
+    if (Value *Res = OptimizePointerDifference(Op1, Op2, II->getType())) {
+      return replaceInstUsesWith(*II, Res);
+    }
+    break;
+  }
   }
   return visitCallSite(II);
 }
